5ffe3b6e1b0be5f19978f58aaa7ecb9e9288abef
SnowyWhite <nick.kibish@gmail.com>
2014-11-06 16:31:29 +0200

    Initial Commit

---
+++ 10Twitts/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  10Twitts
+//
+//  Created by Nick Kibish on 06.11.14.
+//  Copyright (c) 2014 Nick Kibish. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
